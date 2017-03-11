cd03027b33234c14b0f70ac02c1f9c84d5d7d291
aasoma <asoma2691@myhta.org>
2017-03-10 14:28:22 -1000

    I finished all the things

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
+console.log("Hello World");
+var y = 5;
+console.log(y)
+
 function fortuneTeller(){
-
+	var numofcookies = "8 Cookies";//string
+	var cookietype = "Peanut Butter Cookies";//string
+	alert("You will eat " +numofcookies+ "")
 }
 
-function ageCalculator(){
+fortuneTeller();
 
+function ageCalculator(){//function
+	var birthyear = 1991;//number
+	var furuteyear = 10000;//number
+	var howoldwillibe = furuteyear - birthyear;//number
+	alert("I will be either "+ howoldwillibe +" or NN in "+ furuteyear +"")
 }
+ageCalculator();
